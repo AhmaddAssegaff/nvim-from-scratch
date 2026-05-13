ef254dba949b9e9be22f5688863979bc4ceb7ad6
Ahmad <ahmadasgf89@gmail.com>
2026-05-13 21:46:29 +0700

    chore: add which-key

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,8 +7,15 @@ return {
   config = function()
     local builtin = require('telescope.builtin')
 
-    vim.keymap.set('n', '<leader>ff', builtin.find_files)
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {
+          desc = "Find files"
+      })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {
+          desc = "Live grep"
+      })
+      vim.keymap.set("n", "<leader>gf", builtin.git_status, {
+          desc = "Git changed files"
+      })
 
     require('telescope').setup({})
   end
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,20 @@
+return {
+    "folke/which-key.nvim",
+    "nvim-tree/nvim-web-devicons",
+
+
+    config = function()
+        local wk = require("which-key")
+
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+
+        wk.setup({})
+
+        wk.add({
+            { "<leader>f", group = "File" },
+            { "<leader>g", group = "Git" },
+            { "<leader>e", group = "explore" },
+        })
+    end
+}
